da2fb5f3434d08f4f082863e52b841623d4177db
Alex <acorp56000@mail.ru>
2019-02-09 19:16:24 +0300

    жирность заголовков в резервировании

--- protected/views/admin/reserve.php
+++ protected/views/admin/reserve.php
@@ -26,42 +26,42 @@
                 <tbody>
                     <tr>
                         <th scope="row" class="align-middle"><?php echo Questionnaire::getCAMPName(Questionnaire::CAMP_KIROVEC); ?></th>
-                        <td class="text-center">Смена 1
+                        <td class="text-center"><b>Смена 1</b>
                             <?php echo CHtml::activeTextField($model, 'srez_1', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_KIROVEC_1]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_KIROVEC_1]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_KIROVEC_1]['seats'], $shifts[Questionnaire::SHIFT_KIROVEC_1]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 2
+                        <td class="text-center" colspan="2"><b>Смена 2</b>
                             <?php echo CHtml::activeTextField($model, 'srez_2', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_KIROVEC_2]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_KIROVEC_2]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_KIROVEC_2]['seats'], $shifts[Questionnaire::SHIFT_KIROVEC_2]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 3
+                        <td class="text-center" colspan="2"><b>Смена 3</b>
                             <?php echo CHtml::activeTextField($model, 'srez_3', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_KIROVEC_3]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_KIROVEC_3]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_KIROVEC_3]['seats'], $shifts[Questionnaire::SHIFT_KIROVEC_3]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 4
+                        <td class="text-center" colspan="2"><b>Смена 4</b>
                             <?php echo CHtml::activeTextField($model, 'srez_4', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_KIROVEC_4]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_KIROVEC_4]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_KIROVEC_4]['seats'], $shifts[Questionnaire::SHIFT_KIROVEC_4]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 5
+                        <td class="text-center"><b>Смена 5</b>
                             <?php echo CHtml::activeTextField($model, 'srez_5', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_KIROVEC_5]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_KIROVEC_5]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_KIROVEC_5]['seats'], $shifts[Questionnaire::SHIFT_KIROVEC_5]['seats']); ?>
                         </td>
                     </tr>
                     <tr>
                         <th scope="row"class="align-middle"><?php echo Questionnaire::getCAMPName(Questionnaire::CAMP_BLUESCREEN); ?></th>
-                        <td class="text-center">Смена 1
+                        <td class="text-center"><b>Смена 1</b>
                             <?php echo CHtml::activeTextField($model, 'srez_6', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BLUESCREEN_1]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BLUESCREEN_1]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BLUESCREEN_1]['seats'], $shifts[Questionnaire::SHIFT_BLUESCREEN_1]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 2
+                        <td class="text-center" colspan="2"><b>Смена 2</b>
                             <?php echo CHtml::activeTextField($model, 'srez_7', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BLUESCREEN_2]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BLUESCREEN_2]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BLUESCREEN_2]['seats'], $shifts[Questionnaire::SHIFT_BLUESCREEN_2]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 3
+                        <td class="text-center" colspan="2"><b>Смена 3</b>
                             <?php echo CHtml::activeTextField($model, 'srez_8', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BLUESCREEN_3]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BLUESCREEN_3]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BLUESCREEN_3]['seats'], $shifts[Questionnaire::SHIFT_BLUESCREEN_3]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 4
+                        <td class="text-center" colspan="2"><b>Смена 4</b>
                             <?php echo CHtml::activeTextField($model, 'srez_9', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BLUESCREEN_4]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BLUESCREEN_4]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BLUESCREEN_4]['seats'], $shifts[Questionnaire::SHIFT_BLUESCREEN_4]['seats']); ?>
                         </td>
@@ -70,15 +70,15 @@
                     <tr>
                         <th scope="row" class="align-middle"><?php echo Questionnaire::getCAMPName(Questionnaire::CAMP_EAST_4); ?></th>
                         <td>&nbsp;</td>
-                        <td class="text-center" colspan="2">Смена 1
+                        <td class="text-center" colspan="2"><b>Смена 1</b>
                             <?php echo CHtml::activeTextField($model, 'srez_10', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_EAST_1]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_EAST_1]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_EAST_1]['seats'], $shifts[Questionnaire::SHIFT_EAST_1]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 2
+                        <td class="text-center" colspan="2"><b>Смена 2</b>
                             <?php echo CHtml::activeTextField($model, 'srez_11', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_EAST_2]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_EAST_2]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_EAST_2]['seats'], $shifts[Questionnaire::SHIFT_EAST_2]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 3
+                        <td class="text-center" colspan="2"><b>Смена 3</b>
                             <?php echo CHtml::activeTextField($model, 'srez_12', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_EAST_3]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_EAST_3]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_EAST_3]['seats'], $shifts[Questionnaire::SHIFT_EAST_3]['seats']); ?>
                         </td>
@@ -87,19 +87,19 @@
                     <tr>
                         <th scope="row" class="align-middle"><?php echo Questionnaire::getCAMPName(Questionnaire::CAMP_DIAMOND); ?></th>
                         <td>&nbsp;</td>
-                        <td class="text-center" colspan="2">Смена 1
+                        <td class="text-center" colspan="2"><b>Смена 1</b>
                             <?php echo CHtml::activeTextField($model, 'srez_13', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_DIAMOND_1]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_DIAMOND_1]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_DIAMOND_1]['seats'], $shifts[Questionnaire::SHIFT_DIAMOND_1]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 2
+                        <td class="text-center"><b>Смена 2</b>
                             <?php echo CHtml::activeTextField($model, 'srez_14', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_DIAMOND_2]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_DIAMOND_2]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_DIAMOND_2]['seats'], $shifts[Questionnaire::SHIFT_DIAMOND_2]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 3
+                        <td class="text-center"><b>Смена 3</b>
                             <?php echo CHtml::activeTextField($model, 'srez_15', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_DIAMOND_3]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_DIAMOND_3]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_DIAMOND_3]['seats'], $shifts[Questionnaire::SHIFT_DIAMOND_3]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 4
+                        <td class="text-center" colspan="2"><b>Смена 4</b>
                             <?php echo CHtml::activeTextField($model, 'srez_16', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_DIAMOND_4]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_DIAMOND_4]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_DIAMOND_4]['seats'], $shifts[Questionnaire::SHIFT_DIAMOND_4]['seats']); ?>
                         </td>
@@ -108,19 +108,19 @@
                     <tr>
                         <th scope="row" class="align-middle"><?php echo Questionnaire::getCAMPName(Questionnaire::CAMP_BONFIRE); ?></th>
                         <td>&nbsp;</td>
-                        <td class="text-center" colspan="2">Смена 1
+                        <td class="text-center" colspan="2"><b>Смена 1</b>
                             <?php echo CHtml::activeTextField($model, 'srez_17', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BONFIRE_1]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BONFIRE_1]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BONFIRE_1]['seats'], $shifts[Questionnaire::SHIFT_BONFIRE_1]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 2
+                        <td class="text-center"><b>Смена 2</b>
                             <?php echo CHtml::activeTextField($model, 'srez_18', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BONFIRE_2]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BONFIRE_2]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BONFIRE_2]['seats'], $shifts[Questionnaire::SHIFT_BONFIRE_2]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 3
+                        <td class="text-center"><b>Смена 3</b>
                             <?php echo CHtml::activeTextField($model, 'srez_19', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BONFIRE_3]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BONFIRE_3]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BONFIRE_3]['seats'], $shifts[Questionnaire::SHIFT_BONFIRE_3]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 4
+                        <td class="text-center" colspan="2"><b>Смена 4</b>
                             <?php echo CHtml::activeTextField($model, 'srez_20', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_BONFIRE_4]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_BONFIRE_4]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_BONFIRE_4]['seats'], $shifts[Questionnaire::SHIFT_BONFIRE_4]['seats']); ?>
                         </td>
@@ -129,15 +129,15 @@
                     <tr>
                         <th scope="row" class="align-middle"><?php echo Questionnaire::getCAMPName(Questionnaire::CAMP_LIGHTHOUSE); ?></th>
                         <td>&nbsp;</td>
-                        <td class="text-center" colspan="2">Смена 1
+                        <td class="text-center" colspan="2"><b>Смена 1</b>
                             <?php echo CHtml::activeTextField($model, 'srez_21', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_LIGHTHOUSE_1]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_LIGHTHOUSE_1]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_LIGHTHOUSE_1]['seats'], $shifts[Questionnaire::SHIFT_LIGHTHOUSE_1]['seats']); ?>
                         </td>
-                        <td class="text-center" colspan="2">Смена 2
+                        <td class="text-center" colspan="2"><b>Смена 2</b>
                             <?php echo CHtml::activeTextField($model, 'srez_22', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_LIGHTHOUSE_2]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_LIGHTHOUSE_2]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_LIGHTHOUSE_2]['seats'], $shifts[Questionnaire::SHIFT_LIGHTHOUSE_2]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 3
+                        <td class="text-center"><b>Смена 3</b>
                             <?php echo CHtml::activeTextField($model, 'srez_23', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_LIGHTHOUSE_3]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_LIGHTHOUSE_3]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_LIGHTHOUSE_3]['seats'], $shifts[Questionnaire::SHIFT_LIGHTHOUSE_3]['seats']); ?>
                         </td>
@@ -147,19 +147,19 @@
                         <th scope="row"
                             class="align-middle"><?php echo Questionnaire::getCAMPName(Questionnaire::CAMP_FLYGHT); ?></th>
                         <td>&nbsp;</td>
-                        <td class="text-center">Смена 1
+                        <td class="text-center"><b>Смена 1</b>
                             <?php echo CHtml::activeTextField($model, 'srez_24', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_FLYGHT_1]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_FLYGHT_1]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_FLYGHT_1]['seats'], $shifts[Questionnaire::SHIFT_FLYGHT_1]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 2
+                        <td class="text-center"><b>Смена 2</b>
                             <?php echo CHtml::activeTextField($model, 'srez_25', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_FLYGHT_2]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_FLYGHT_2]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_FLYGHT_2]['seats'], $shifts[Questionnaire::SHIFT_FLYGHT_2]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 3
+                        <td class="text-center"><b>Смена 3</b>
                             <?php echo CHtml::activeTextField($model, 'srez_26', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_FLYGHT_3]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_FLYGHT_3]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_FLYGHT_3]['seats'], $shifts[Questionnaire::SHIFT_FLYGHT_3]['seats']); ?>
                         </td>
-                        <td class="text-center">Смена 4
+                        <td class="text-center"><b>Смена 4</b>
                             <?php echo CHtml::activeTextField($model, 'srez_27', array('class' => 'form-control text-center', 'data-mask'=>'0123456789', 'maxlength'=>3, 'data-max'=>$shifts[Questionnaire::SHIFT_FLYGHT_4]['seats'], 'data-toggle'=>'tooltip', 'title'=>'Значение от 0 из '.$shifts[Questionnaire::SHIFT_FLYGHT_4]['seats'])); ?>
                             <?php echo SiteService::templateSeatsCount($seats[Questionnaire::SHIFT_FLYGHT_4]['seats'], $shifts[Questionnaire::SHIFT_FLYGHT_4]['seats']); ?>
                         </td>
