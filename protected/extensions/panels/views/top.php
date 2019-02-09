3878bc596a6f729f370275b63e1cceef70e04724
Alex <acorp56000@mail.ru>
2019-02-09 18:37:47 +0300

    кнопка

--- protected/extensions/panels/views/top.php
+++ protected/extensions/panels/views/top.php
@@ -6,8 +6,15 @@
     </div>
     <div class="row">
         <div class="col-8">
-            <a class="nav-item btn btn-primary mr-2 <?php echo(in_array($action, array('addstatement')) ? 'active' : ''); ?>" href="<?php echo Yii::app()->createUrl('/site/addstatement'); ?>">Подать заявку</a>
-            <a class="nav-item btn btn-primary mr-2 <?php echo((($controller == 'profile') && in_array($action, array('index', 'bid'))) ? 'active' : ''); ?>" href="<?php echo Yii::app()->createUrl('/profile'); ?>">Мои заявки</a>
+            <a class="nav-item btn btn-primary mr-2 <?php echo(in_array($action, array('addstatement')) ? 'active' : ''); ?>"
+               href="<?php echo Yii::app()->createUrl('/site/addstatement'); ?>">Подать заявку</a>
+            <a class="nav-item btn btn-primary mr-2 <?php echo((($controller == 'profile') && in_array($action, array('index', 'bid'))) ? 'active' : ''); ?>"
+               href="<?php echo Yii::app()->createUrl('/profile'); ?>">Мои заявки</a>
+
+            <?php if (Yii::app()->user->role == User::ROLE_ADMIN) { ?>
+                <a class="nav-item btn btn-primary mr-2 <?php echo(($controller == 'admin') && in_array($action, array('index', 'bid', 'stat', 'reserve')) ? 'active' : ''); ?>"
+                   href="<?php echo Yii::app()->createUrl('/admin'); ?>">Админ-панель</a>
+            <?php } ?>
         </div>
         <div class="col-4 text-right">
             <a class="nav-item btn btn-primary" href="<?php echo Yii::app()->createUrl('/auth/logout'); ?>">Выход</a>
