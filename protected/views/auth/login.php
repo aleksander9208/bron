dbc4544914a366ab99c851515d659c7c9e62739b
san58 <multipass>
2019-02-11 23:15:06 +0300

    Форма авторизации + текстовки

--- protected/views/auth/login.php
+++ protected/views/auth/login.php
@@ -1,25 +1,26 @@
 <div id="z_page_auth" class="row">
     <div class="col">
-        <h3 class="mb-2">Регистрация заявок на&nbsp;отдых в&nbsp;летних лагерях</h3>
-        <p>У&nbsp;нас на&nbsp;сайте можно подать заявки на&nbsp;отдых Ваших детей в&nbsp;летних лагерях на&nbsp;период <b>лета 2019 </b> года.</p>
-        <p>Для входа в&nbsp;&laquo;Личный кабинет&raquo;, воспользуйтесь формой авторизации ниже. В&nbsp;&laquo;Личном кабинет&raquo; всегда можно уточнит текущий статус поданных заявок, отказаться от&nbsp;их, либо добавить новые заявки.</p>
+        <h3 class="mb-2">Электронная заявка на&nbsp;бронирование путевки в&nbsp;детский лагерь отдыха.</h3>
+        <p>Чтобы заполнить электронную форму заявки для бронирования путевки в&nbsp;детский лагерь отдыха в&nbsp;период летней кампании 2019&nbsp;года, нажмите кнопку &laquo;Подать заявку&raquo;. После подачи заявки, автоматически будет сформирован &laquo;Личный</p>
+        <a href="<?php echo Yii::app()->createAbsoluteUrl('/site/addstatement'); ?>" class="btn btn-block btn-success mx-auto mw-50 w-50">Подать заявку</a>
+        <p>Для уточнения текущего статуса поданных заявок, отказа от&nbsp;них, подачи новых заявок на&nbsp;бронирование, пожалуйста, пройдите авторизацию и&nbsp;перейдите в &laquo;Личный кабинет&raquo;.</p>
         <div class="card mw-50 w-50 mx-auto mb-2">
             <h5 class="card-header">Авторизация в личный кабинет</h5>
             <div class="card-body">
                 <form id="z_auth_form" class="needs-validation" method="post">
                     <div class="form-group">
-                        <label for="z_auth_user_fio" class="col-form-label">ФИО:</label>
+                        <label for="z_auth_user_fio" class="col-form-label">ФИО родителя:</label>
                         <?php echo CHtml::activeTextField($model, 'username', array('placeholder' => 'Введите ФИО', 'class' => 'form-control', 'required'=>'required', 'id'=>'z_auth_user_fio', 'data-mask'=>' абвгдеёжзийклмнопрстуфхцчшщъыьэюя-')); ?>
                         <div class="invalid-feedback">Укажите ФИО подавшего заявку</div>
                     </div>
                     <div class="form-group">
-                        <label for="z_auth_user_phone" class="col-form-label">Телефон:</label>
+                        <label for="z_auth_user_phone" class="col-form-label">Телефон родителя:</label>
                         <div class="input-group mb-2">
                             <div class="input-group-prepend">
                                 <div class="input-group-text">+7</div>
                             </div>
                             <?php echo CHtml::activeTextField($model, 'password', array('placeholder' => 'Введите телефон', 'class' => 'form-control', 'required'=>'required', 'id'=>'z_auth_user_phone', 'maxlength'=>15, 'data-mask'=>'0123456789')); ?>
-                            <div class="invalid-feedback">Укажите телефон подавшего заявку (Например: 9051234567)</div>
+                            <div class="invalid-feedback">Укажите телефон подавшего заявку (Например: (905) 123-45-67)</div>
                         </div>
                     </div>
                     <div class="form-group mb-0">
@@ -29,7 +30,6 @@
                 </form>
             </div>
         </div>
-        <p>Если Вы&nbsp;ранее не&nbsp;подавали заявки на&nbsp;отдых в&nbsp;летних лагерях и&nbsp;у&nbsp;Вас нет учетной записи для &laquo;Личного кабинета&raquo;, то&nbsp;перейдите на&nbsp;форму анкеты подачи заявки. После подачи заявки, для Вас будет автоматически сформирована учетная запись.</p>
-        <a href="<?php echo Yii::app()->createAbsoluteUrl('/site/addstatement'); ?>" class="btn btn-block btn-success mx-auto mw-50 w-50">Подать заявку</a>
+
     </div>
 </div>
