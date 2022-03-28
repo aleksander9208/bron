192fe001ce354552de4a81db18c3632756938fd5
aleksandrostroushko <a.ostroushko@zebrains.tearm>
2022-03-28 15:20:05 +0300

    актуализация репозитория

---
+++ public_html/test.php
@@ -0,0 +1,76 @@
+<?php
+
+<pre style="displa2y: none;" alt="arResult">Array
+    (
+        [0] =&gt; Array
+(
+    [id] =&gt; 1
+            [camp] =&gt; «Кировец»
+[seats] =&gt; 192
+            [min_age] =&gt; 6
+            [max_age] =&gt; 17
+            [dlo] =&gt;
+        )
+
+    [1] =&gt; Array
+(
+    [id] =&gt; 2
+            [camp] =&gt; «Голубой экран»
+[seats] =&gt; 112
+            [min_age] =&gt; 6
+            [max_age] =&gt; 10
+            [dlo] =&gt;
+        )
+
+    [2] =&gt; Array
+(
+    [id] =&gt; 3
+            [camp] =&gt; «Восток-4»
+[seats] =&gt; 154
+            [min_age] =&gt; 6
+            [max_age] =&gt; 17
+            [dlo] =&gt;
+        )
+
+    [3] =&gt; Array
+(
+    [id] =&gt; 4
+            [camp] =&gt; «Алмаз»
+[seats] =&gt; 164
+            [min_age] =&gt; 11
+            [max_age] =&gt; 17
+            [dlo] =&gt;
+        )
+
+    [4] =&gt; Array
+(
+    [id] =&gt; 5
+            [camp] =&gt; «Костер»
+[seats] =&gt; 196
+            [min_age] =&gt; 6
+            [max_age] =&gt; 17
+            [dlo] =&gt;
+        )
+
+    [5] =&gt; Array
+(
+    [id] =&gt; 6
+            [camp] =&gt; «Маяк»
+[seats] =&gt; 88
+            [min_age] =&gt; 6
+            [max_age] =&gt; 17
+            [dlo] =&gt;
+        )
+
+    [6] =&gt; Array
+(
+    [id] =&gt; 7
+            [camp] =&gt; «Полет»
+[seats] =&gt; 132
+            [min_age] =&gt; 6
+            [max_age] =&gt; 17
+            [dlo] =&gt;
+        )
+
+)
+</pre>
